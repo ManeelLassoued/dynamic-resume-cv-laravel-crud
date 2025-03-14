c4f2fcc61eeb7e6b613fe4138d482a40c9bd32b2
ManeelLassoued <ing.manellassoued@gmail.com>
2025-03-14 01:10:10 +0100

    change app

---
+++ app/Models/User.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    protected $guarded = ['id'];
+
+
+    public function contactInformation(): HasMany
+    {
+        return $this->hasMany(ContactInformation::class);
+    }
+
+    public function education(): HasMany
+    {
+        return $this->hasMany(Education::class);
+    }
+
+    public function experiences(): HasMany
+    {
+        return $this->hasMany(Experience::class);
+    }
+
+    public function interests(): HasMany
+    {
+        return $this->hasMany(Interests::class);
+    }
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Projects::class);
+    }
+
+    public function languages(): HasMany
+    {
+        return $this->hasMany(Languages::class);
+    }
+
+    public function skills(): HasMany
+    {
+        return $this->hasMany(Skills::class);
+    }
+
+    public function hasRole($role)
+    {
+        // Assuming you have a 'role' column in your users table
+        return $this->role === $role;
+    }
+}
